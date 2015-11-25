9358defea3681cac9668450a82d513fc2a03baa2
Mark Westenberg <mwestenberg@markbookpro.local>
2015-11-25 10:15:48 +0100

    Initial Commit

---
+++ Sony Remote WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  Sony Remote WatchKit Extension
+//
+//  Created by Mark Westenberg on 25/11/15.
+//  Copyright © 2015 Mark Westenberg. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
