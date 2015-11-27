d1ab181be7bc1585352c6a31d370726427eb5c66
Timon van Rooijen <timonvanrooijen@timons-macbook-pro.local>
2015-11-27 22:49:24 +0100

    update

--- Sony Remote/Connector/Connector.h
+++ Sony Remote/Connector/Connector.h
@@ -9,16 +9,22 @@
 #import <Foundation/Foundation.h>
 #import "TV.h"
 
-@interface Connector : NSObject
+@interface Connector : NSObject {
+    void (^_completionHandler)(BOOL result);
+    
+}
 
 #define APIVERSION  @"1.0"
 #define tv_ip       @"192.168.1.115"
+#define client      @"MacOSX:5EF3FF8C-1EB0-4E1D-B53C-038969B6BC7F"
+#define nickname    @"Macbook OSX"
 
 @property (nonatomic, assign) int               idCounter;
 @property (nonatomic, strong) TV                *tv;
 @property (nonatomic, strong) NSString          *authCookie;
 
 //Geeft NIL terug als niet geautoriseerd. Dan nog een x aanroepen met pincode gevuld.
-- (TV *) initializeTV:(NSString *)pin;
+- (void) initializeTV:(NSString *)pin withCompletionHandler:(void(^)(int))handler;
+- (void) sendRemoteKey:(NSString *)key;
 
 @end
